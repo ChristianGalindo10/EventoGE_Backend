43cc4284d4f1d37b9835814cf83496e9e4c28d0c
ChristianGalindo10 <cygc1@hotmail.com>
2023-06-28 12:27:56 -0500

    Final2

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,26 +7,26 @@ import config from '../config';
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      inject: [config.KEY],
-      useFactory: (configService: ConfigType<typeof config>) => {
-        const { host, name, user, password, port, schema } =
-          configService.postgres;
-        return {
-          type: 'postgres',
-          host,
-          database: name,
-          schema,
-          username: user,
-          password,
-          port,
-          synchronize: true,
-          autoLoadEntities: true,
-          //entities: ["src/**/*.entity.ts"],
-        };
-      },
-      // type: 'postgres',
-      // url: 'postgres://postgres1:SaHn7weFP85XUfib7S6oWI1zqqdbhFGe@dpg-cido3gp5rnukltkjf7hg-a.oregon-postgres.render.com/gearelectric_evento?ssl=true'
+    TypeOrmModule.forRoot({
+      // inject: [config.KEY],
+      // useFactory: (configService: ConfigType<typeof config>) => {
+      //   const { host, name, user, password, port, schema } =
+      //     configService.postgres;
+      //   return {
+      //     type: 'postgres',
+      //     host,
+      //     database: name,
+      //     schema,
+      //     username: user,
+      //     password,
+      //     port,
+      //     synchronize: true,
+      //     autoLoadEntities: true,
+      //     //entities: ["src/**/*.entity.ts"],
+      //   };
+      // },
+      type: 'postgres',
+      url: 'postgres://postgres1:SaHn7weFP85XUfib7S6oWI1zqqdbhFGe@dpg-cido3gp5rnukltkjf7hg-a.oregon-postgres.render.com/gearelectric_evento?ssl=true'
     }),
   ],
   exports: [TypeOrmModule],
