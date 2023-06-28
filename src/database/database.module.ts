f94d6b62db47cb3f781f040aab875036a3772e4d
ChristianGalindo10 <cygc1@hotmail.com>
2023-06-28 12:42:00 -0500

    fix db connection

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -7,26 +7,26 @@ import config from '../config';
 @Global()
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      // inject: [config.KEY],
-      // useFactory: (configService: ConfigType<typeof config>) => {
-      //   const { host, name, user, password, port, schema } =
-      //     configService.postgres;
-      //   return {
-      //     type: 'postgres',
-      //     host,
-      //     database: name,
-      //     schema,
-      //     username: user,
-      //     password,
-      //     port,
-      //     synchronize: true,
-      //     autoLoadEntities: true,
-      //     //entities: ["src/**/*.entity.ts"],
-      //   };
-      // },
-      type: 'postgres',
-      url: 'postgres://postgres1:SaHn7weFP85XUfib7S6oWI1zqqdbhFGe@dpg-cido3gp5rnukltkjf7hg-a.oregon-postgres.render.com/gearelectric_evento?ssl=true'
+    TypeOrmModule.forRootAsync({
+      inject: [config.KEY],
+      useFactory: (configService: ConfigType<typeof config>) => {
+        const { host, name, user, password, port, schema } =
+          configService.postgres;
+        return {
+          type: 'postgres',
+          host,
+          database: name,
+          schema,
+          username: user,
+          password,
+          port,
+          synchronize: true,
+          autoLoadEntities: true,
+          //entities: ["src/**/*.entity.ts"],
+        };
+      },
+      // type: 'postgres',
+      // url: 'postgres://unzqgyjk:gyRJDSCjS4btdsER6aWdfYD8OQ2n-f-n@mahmud.db.elephantsql.com/unzqgyjk?ssl=true'
     }),
   ],
   exports: [TypeOrmModule],
